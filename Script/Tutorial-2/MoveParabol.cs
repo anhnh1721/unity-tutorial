f77883a01e4aa748225860daa4691e0bdc2cdffe
DESKTOP-AV4NGBM\Anhnh1721 <anhnh1721@gmail.com>
2017-09-19 16:03:59 +0700

    Tut-2 + 3

---
+++ Script/Tutorial-2/MoveParabol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveParabol : MonoBehaviour {
+
+    public float timeMove;
+    public float height;
+
+    public Transform target;
+
+    private bool isMoving;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
+            StartCoroutine(MoveParabola());
+	}
+
+    IEnumerator MoveParabola()
+    {
+        isMoving = true;
+
+        var startPosition = transform.position;
+        var timer = 0f;
+
+        while(timer <= 1f)
+        {
+            var heightTemp = Mathf.Sin(Mathf.PI * timer) * height;
+            transform.position = Vector3.Lerp(startPosition, target.position, timer) + Vector3.up * heightTemp;
+
+            timer += Time.deltaTime / timeMove;
+            yield return null;
+        }
+
+        transform.position = target.position;
+
+        isMoving = false;
+    }
+}
---
+++ Script/Tutorial-3/CameraFollower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothTime = 0.25f;
+    public Vector3 velocity = Vector3.zero;
+
+    //void Update () {
+    //       transform.position = target.localPosition + offset;
+    //}
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
